87c175a6484fc5bb994d48412ee96e4a75cba7b9
katerith <thanouaikaterini@gmail.com>
2022-02-08 20:36:16 +0200

    node CRUD API

--- BackEnd API (node.js)/index.js
+++ BackEnd API (node.js)/index.js
@@ -10,9 +10,9 @@ app.use(bodyParser.json());
 app.post('/contacts', async (req, res) => {
     try {
         const [contactIndex] = await db.createContact(req.body);
-        console.log('contactIndex', contactIndex)
-        console.log('................................................')
         res.status(201).json({ id: contactIndex })
+        console.log(`Contact No.${contactIndex} was successfully created`)
+        console.log('................................................')
     } catch(err) {
         console.log('err', err.message)
     }
@@ -22,7 +22,8 @@ app.get('/contacts', async (req, res) => {
     try {
         const contacts = await db.getAllContacts();
         res.status(200).json({ contacts })
-        console.log('contacts', contacts)
+        console.log('Your contacts are:',)
+        contacts.map(contact => console.log(JSON.stringify(contact)))
         console.log('................................................')
     } catch(err) {
         console.log('err', err.message)
@@ -31,9 +32,9 @@ app.get('/contacts', async (req, res) => {
 
 app.get('/contacts/:id', async (req, res) => {
     try {
-        const contact = await db.getContactDetails(req.params.id);
+        const [contact] = await db.getContactDetails(req.params.id);
         res.status(200).json({ contact })
-        console.log('contact', contact)
+        console.log(`The Contact No.${req.params.id} is ${JSON.stringify(contact)}`)
         console.log('................................................')
     } catch(err) {
         console.log('err', err.message)
@@ -44,7 +45,7 @@ app.patch('/contacts/:id', async (req, res) => {
     try {
         const updatedContact = await db.updateContact(req.params.id, req.body);
         res.status(200).json({ updatedContact })
-        console.log('updatedContact', updatedContact)
+        console.log(`The Contact No.${req.params.id} was updated to ${JSON.stringify(req.body)}`)
         console.log('................................................')
     } catch(err) {
         console.log('err', err.message)
@@ -55,7 +56,7 @@ app.delete('/contacts/:id', async (req, res) => {
     try {
         await db.deleteContact(req.params.id);
         res.status(200).json({ success: true });
-        console.log(`Contact No.${req.params.id} was succesfuly deleted`)
+        console.log(`The Contact No.${req.params.id} was succesfuly deleted`)
         console.log('................................................')
     } catch(err) {
         console.log('err', err.message)
